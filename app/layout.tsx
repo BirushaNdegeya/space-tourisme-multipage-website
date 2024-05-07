c4b08e92fff40dab2adb0a6a0603fe02b1e9e03d
BirushaNdegeya <birushandegeya@gmail.com>
2024-05-07 19:18:30 +0200

    add background image to homePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const nunito = Nunito_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "space tourism multipage website",
   description: "challenge from frontend mentor coded by Birusha Ndegeya",
+  icons: '/favicon-32x32.png'
 };
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import HomeDesktopImage from '@/public/home/background-home-desktop.jpg';
+import HomeMobileImage from '@/public/home/background-home-mobile.jpg';
+import HomeTabletImage from '@/public/home/background-home-tablet.jpg';
+
 const Home = () => {
+  let bg: string = HomeDesktopImage;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-image-home">
       <h1>SPACE TOURISME MULTIPAGE WEBSITE</h1>
     </main>
   );
